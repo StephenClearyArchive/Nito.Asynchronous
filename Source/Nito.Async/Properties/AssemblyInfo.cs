28c2d4a953924c9ee712a790f5d0d541f4acedfc
Stephen Cleary <StephenCleary@users.noreply.github.com>
2009-05-30 14:40:47 +0000

    Set to version 2.0 and removed explicit AssemblyFileVersion

--- Source/Nito.Async/Properties/AssemblyInfo.cs
+++ Source/Nito.Async/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+
