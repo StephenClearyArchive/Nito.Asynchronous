2c9aee5e78f04df87232feed807e62f7becd9889
shammah_cp <SND\shammah_cp>
2009-08-29 22:37:18 +0000

    Added very basic unit tests for ScopedSynchronizationContext and SynchronizationContextRegister.

---
+++ trunk/Source/Nito.Async.UnitTests/ScopedSynchronizationContextUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Nito.Async;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ScopedSynchronizationContextUnitTests
+    {
+        [TestMethod]
+        public void TestBasicFunctionality()
+        {
+            var oldSyncContext = SynchronizationContext.Current;
+            var newSyncContext = new SynchronizationContext();
+
+            using (var x = new ScopedSynchronizationContext(newSyncContext))
+            {
+                Assert.AreSame(newSyncContext, SynchronizationContext.Current, "ScopedSynchronizationContext did not replace SynchronizationContext.Current");
+            }
+
+            Assert.AreSame(oldSyncContext, SynchronizationContext.Current, "ScopedSynchronizationContext did not restore the old SynchronizationContext.Current");
+        }
+    }
+}
