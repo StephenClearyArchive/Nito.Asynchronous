ca1de67914bf34147f7a2dc19918fc1b0941b99d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2009-08-30 21:45:26 +0000

    Even more unit testing work...

---
+++ Source/Nito.Async.UnitTests/EventHandlers.AsyncResultEventArgsUnitTests.cs
@@ -0,0 +1,76 @@
+﻿using Nito.Async;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AsyncResultEventArgsUnitTests
+    {
+        [TestMethod]
+        public void TestFailureResult()
+        {
+            Exception error = new Exception();
+
+            var test = new AsyncResultEventArgs<int>(error);
+            Assert.IsFalse(test.Cancelled, "Failed event args should not be cancelled");
+            Assert.AreSame(error, test.Error, "Failed event args did not preserve exception");
+        }
+
+        [TestMethod]
+        public void TestFailureResultWithUserState()
+        {
+            Exception error = new Exception();
+            object state = new object();
+
+            var test = new AsyncResultEventArgs<int>(0, error, false, state);
+            Assert.IsFalse(test.Cancelled, "Failed event args should not be cancelled");
+            Assert.AreSame(error, test.Error, "Failed event args did not preserve exception");
+            Assert.AreSame(state, test.UserState, "Failed event args did not preserve user state");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TargetInvocationException))]
+        public void TestFailureResultReadingResult()
+        {
+            var test = new AsyncResultEventArgs<int>(new Exception());
+            Trace.WriteLine(test.Result);
+        }
+
+        [TestMethod]
+        public void TestCancelledResult()
+        {
+            object state = new object();
+
+            var test = new AsyncResultEventArgs<int>(0, null, true, state);
+            Assert.IsTrue(test.Cancelled, "Cancelled event args should be cancelled");
+            Assert.IsNull(test.Error, "Cancelled event args should not preserve exception");
+            Assert.AreSame(state, test.UserState, "Cancelled event args did not preserve user state");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestCancelledReadingResult()
+        {
+            var test = new AsyncResultEventArgs<int>(0, null, true, null);
+            Trace.WriteLine(test.Result);
+        }
+
+        [TestMethod]
+        public void TestSuccessfulResult()
+        {
+            object state = new object();
+
+            var test = new AsyncResultEventArgs<int>(13, null, false, state);
+            Assert.IsFalse(test.Cancelled, "Successful event args should not be cancelled");
+            Assert.IsNull(test.Error, "Successful event args should not preserve exception");
+            Assert.AreSame(state, test.UserState, "Successful event args did not preserve user state");
+            Assert.AreEqual(13, test.Result, "Successful event args did not preserve result");
+        }
+    }
+}
---
+++ Source/Nito.Async.UnitTests/EventHandlers.SynchronizeUnitTests.cs
@@ -0,0 +1,118 @@
+﻿using Nito.Async;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class EventHandlersSynchronizeUnitTests
+    {
+        [TestMethod]
+        public void TestSuccessfulAction()
+        {
+            bool sawAction = false;
+            object state = new object();
+            bool argsCancelled = true;
+            Exception argsError = new Exception();
+            object argsState = null;
+
+            Sync.InvokeAndCallback(() => { sawAction = true; }, (x) =>
+                {
+                    argsCancelled = x.Cancelled;
+                    argsError = x.Error;
+                    argsState = x.UserState;
+                }, state);
+
+            Assert.IsTrue(sawAction, "Action did not run");
+            Assert.IsFalse(argsCancelled, "Completed arguments indicate action was cancelled");
+            Assert.IsNull(argsError, "Completed arguments indicate an error");
+            Assert.AreSame(state, argsState, "User state not preserved");
+        }
+
+        [TestMethod]
+        public void TestFailedAction()
+        {
+            bool sawAction = false;
+            Exception actionException = new Exception();
+            object state = new object();
+            bool argsCancelled = true;
+            Exception argsError = null;
+            object argsState = null;
+
+            Sync.InvokeAndCallback(() => { sawAction = true; throw actionException; }, (x) =>
+            {
+                argsCancelled = x.Cancelled;
+                argsError = x.Error;
+                argsState = x.UserState;
+            }, state);
+
+            Assert.IsTrue(sawAction, "Action did not run");
+            Assert.IsFalse(argsCancelled, "Failed arguments indicate action was cancelled");
+            Assert.AreSame(actionException, argsError, "Failed arguments did not preserve error");
+            Assert.AreSame(state, argsState, "User state not preserved");
+        }
+
+        [TestMethod]
+        public void TestSuccessfulFunc()
+        {
+            bool sawAction = false;
+            object state = new object();
+            bool argsCancelled = true;
+            Exception argsError = new Exception();
+            object argsState = null;
+            int argsResult = 0;
+
+            Sync.InvokeAndCallback(() => { sawAction = true; return 13; }, (x) =>
+            {
+                argsCancelled = x.Cancelled;
+                argsError = x.Error;
+                argsState = x.UserState;
+                argsResult = x.Result;
+            }, state);
+
+            Assert.IsTrue(sawAction, "Action did not run");
+            Assert.IsFalse(argsCancelled, "Completed arguments indicate action was cancelled");
+            Assert.IsNull(argsError, "Completed arguments indicate an error");
+            Assert.AreSame(state, argsState, "User state not preserved");
+            Assert.AreEqual(13, argsResult, "Result not returned");
+        }
+
+        [TestMethod]
+        public void TestFailedFunc()
+        {
+            bool sawAction = false;
+            Exception actionException = new Exception();
+            object state = new object();
+            bool argsCancelled = true;
+            Exception argsError = null;
+            object argsState = null;
+            Exception argsResultException = null;
+
+            Sync.InvokeAndCallback(() => { sawAction = true; throw actionException; return 13; }, (x) =>
+            {
+                argsCancelled = x.Cancelled;
+                argsError = x.Error;
+                argsState = x.UserState;
+                try
+                {
+                    Trace.WriteLine(x.Result);
+                }
+                catch (Exception ex)
+                {
+                    argsResultException = ex;
+                }
+            }, state);
+
+            Assert.IsTrue(sawAction, "Action did not run");
+            Assert.IsFalse(argsCancelled, "Failed arguments indicate action was cancelled");
+            Assert.AreSame(actionException, argsError, "Failed arguments did not preserve error");
+            Assert.AreSame(state, argsState, "User state not preserved");
+            Assert.IsInstanceOfType(argsResultException, typeof(TargetInvocationException), "Failed arguments allowed reading result");
+        }
+    }
+}
