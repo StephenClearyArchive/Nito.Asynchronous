dfa5ff099ad36592388215265dab58cb2102b46a
Stephen Cleary <StephenCleary@users.noreply.github.com>
2009-09-01 12:03:29 +0000

    Added ActionThread checks in DoSynchronously, and the first unit tests for Timer.

---
+++ Source/Nito.Async.UnitTests/Timer.UnitTests.cs
@@ -0,0 +1,45 @@
+﻿using Nito.Async;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TimerUnitTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Constructor_SynchronizationContextNotSupportingSynchronized_ThrowsInvalidOperationException()
+        {
+            using (ScopedSynchronizationContext x = new ScopedSynchronizationContext(null))
+            {
+                using (Timer timer = new Timer())
+                {
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Enabled_AfterConstruction_IsFalse()
+        {
+            bool enabled = true;
+
+            using (ActionThread thread = new ActionThread())
+            {
+                thread.Start();
+                thread.DoSynchronously(() =>
+                    {
+                        using (Timer timer = new Timer())
+                        {
+                            enabled = timer.Enabled;
+                        }
+                    });
+            }
+
+            Assert.IsFalse(enabled, "Timer.Enabled should initially be false");
+        }
+    }
+}
--- Source/Nito.Async/ActionThread.cs
+++ Source/Nito.Async/ActionThread.cs
@@ -140,8 +140,7 @@ namespace Nito.Async
         /// <param name="timeout">The time to wait for <paramref name="action"/> to execute.</param>
         /// <returns><c>true</c> if <paramref name="action"/> executed completely; <c>false</c> if there was a timeout.</returns>
         /// <remarks>
-        /// <para>Be careful when using short timeout values; the <paramref name="action"/> delegate may not be scheduled for work immediately.</para>
-        /// <para>This method may only be called after the thread has been started.</para>
+        /// <para>Be careful when using short timeout values; the <paramref name="action"/> delegate may not be scheduled for work immediately if the thread already has other work queued.</para>
         /// </remarks>
         public bool DoSynchronously(Action action, TimeSpan timeout)
         {
@@ -157,10 +156,17 @@ namespace Nito.Async
         /// </summary>
         /// <param name="action">The work to do. This delegate may not throw an exception.</param>
         /// <remarks>
-        /// <para>This method may only be called after the thread has been started.</para>
+        /// <para>This method may not be called before the thread has started (see <see cref="Start"/>) or after the thread has joined (see <see cref="Join"/>).</para>
         /// </remarks>
         public void DoSynchronously(Action action)
         {
+            // This test actually has a race condition; it's not possible to fully detect all conditions
+            if (this.thread.ThreadState == ThreadState.Unstarted || this.thread.ThreadState == ThreadState.Stopped)
+            {
+                // If we went ahead and queued the work, it probably would result in a deadlock
+                throw new ThreadStateException("ActionThread.DoSynchronously can only queue work to a running thread.");
+            }
+
             this.DoSynchronously(action, TimeSpan.FromMilliseconds(-1));
         }
 
