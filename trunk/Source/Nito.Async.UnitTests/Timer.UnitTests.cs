7d14f4fb2cfc4d3bf3759d492b4c8e420e7ab824
shammah_cp <SND\shammah_cp>
2009-09-02 11:33:03 +0000

    More unit tests, and moved SyncContext verification after the capture.

--- trunk/Source/Nito.Async.UnitTests/Timer.UnitTests.cs
+++ trunk/Source/Nito.Async.UnitTests/Timer.UnitTests.cs
@@ -12,7 +12,7 @@ namespace UnitTests
     {
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
-        public void Constructor_SynchronizationContextNotSupportingSynchronized_ThrowsInvalidOperationException()
+        public void Constructor_SyncContextNotSupportingSynchronized_ThrowsInvalidOperationException()
         {
             using (ScopedSynchronizationContext x = new ScopedSynchronizationContext(null))
             {
@@ -23,7 +23,7 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void Enabled_AfterConstruction_IsFalse()
+        public void Timer_AfterConstruction_IsDisabled()
         {
             bool enabled = true;
 
@@ -40,6 +40,69 @@ namespace UnitTests
             }
 
             Assert.IsFalse(enabled, "Timer.Enabled should initially be false");
+        }
+
+        [TestMethod]
+        public void Timer_AfterSetSingleShot_IsEnabled()
+        {
+            bool enabled = true;
+
+            using (ActionThread thread = new ActionThread())
+            {
+                thread.Start();
+                thread.DoSynchronously(() =>
+                {
+                    using (Timer timer = new Timer())
+                    {
+                        timer.SetSingleShot(TimeSpan.FromMilliseconds(50));
+                        enabled = timer.Enabled;
+                    }
+                });
+            }
+
+            Assert.IsTrue(enabled, "Timer.Enabled should be true");
+        }
+
+        [TestMethod]
+        public void TimerType_AfterSetSingleShot_IsSingleShot()
+        {
+            bool autoReset = true;
+
+            using (ActionThread thread = new ActionThread())
+            {
+                thread.Start();
+                thread.DoSynchronously(() =>
+                {
+                    using (Timer timer = new Timer())
+                    {
+                        timer.SetSingleShot(TimeSpan.FromMilliseconds(50));
+                        autoReset = timer.AutoReset;
+                    }
+                });
+            }
+
+            Assert.IsFalse(autoReset, "Timer should be single-shot");
+        }
+
+        [TestMethod]
+        public void TimerInterval_AfterSetSingleShot_IsSetToArgument()
+        {
+            TimeSpan interval = default(TimeSpan);
+
+            using (ActionThread thread = new ActionThread())
+            {
+                thread.Start();
+                thread.DoSynchronously(() =>
+                {
+                    using (Timer timer = new Timer())
+                    {
+                        timer.SetSingleShot(TimeSpan.FromMilliseconds(50));
+                        interval = timer.Interval;
+                    }
+                });
+            }
+
+            Assert.AreEqual(TimeSpan.FromMilliseconds(50), interval, "Timer should have its Interval set");
         }
     }
 }
