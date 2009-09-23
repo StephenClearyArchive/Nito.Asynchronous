daaf39953e14eec5f62c00b6d7e9828f43581303
shammah_cp <SND\shammah_cp>
2009-09-23 00:45:24 +0000

    Changed AssemblyInfos to version 1.4

--- trunk/Source/Nito.Async.Sockets/Properties/AssemblyInfo.cs
+++ trunk/Source/Nito.Async.Sockets/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
 
