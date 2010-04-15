d8924f1229b7de0da8a24778e21913508ed814d0
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-04-15 18:15:16 +0000

    Updated to VS2010 toolset (note that Nito.Async still targets .NET 3.5 SP1).

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,3 @@
 @echo off
-%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild.exe /nologo /fl1 /flp1:v=diag
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /nologo /fl1 /flp1:v=diag
 pause
