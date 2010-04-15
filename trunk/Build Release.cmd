547abff19f076351e5ce202d7e5680077c631411
shammah_cp <SND\shammah_cp>
2010-04-15 18:15:16 +0000

    Updated to VS2010 toolset (note that Nito.Async still targets .NET 3.5 SP1).

--- trunk/Build Release.cmd
+++ trunk/Build Release.cmd
@@ -1,3 +1,3 @@
 @echo off
-%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild.exe /t:BuildRelease /nologo /p:Version=1.4 /fl1 /flp1:v=diag
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /t:BuildRelease /nologo /p:Version=1.4 /fl1 /flp1:v=diag
 pause
