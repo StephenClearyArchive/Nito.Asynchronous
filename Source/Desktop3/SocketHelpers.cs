dbb1ab297f5b04ae67b3c3eb22210d045b5370b2
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-10-18 15:26:16 +0000

    Refactored BeginEndAsyncServerChildTcpSocket to match BeginEndAsyncClientTcpSocket.

---
+++ Source/Desktop3/SocketHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nito.Communication
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
+    using Async;
+
+    public static class SocketHelpers
+    {
+        public static IList<ArraySegment<byte>> RemainingBuffers(IList<ArraySegment<byte>> buffers, int bytesWritten)
+        {
+            var ret = new List<ArraySegment<byte>>();
+            int index;
+            for (index = 0; index != buffers.Count && bytesWritten != 0; ++index)
+            {
+                var buffer = buffers[index];
+                if (bytesWritten >= buffer.Count)
+                {
+                    bytesWritten -= buffer.Count;
+                }
+                else
+                {
+                    ret.Add(new ArraySegment<byte>(buffer.Array, buffer.Offset + bytesWritten, buffer.Count - bytesWritten));
+                    bytesWritten = 0;
+                }
+            }
+
+            ret.AddRange(buffers.Skip(index));
+            return ret;
+        }
+    }
+}
