25b479a255791a188737fb28bd4fed7e5c794650
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-10-19 14:03:42 +0000

    Removed sockets and serial port support (moving to Nito.Communication).

--- Source/Desktop3/CallbackOnErrorsOnly.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Nito.Communication
-{
-    /// <summary>
-    /// This is a special class that may be passed to <c>WriteAsync</c> methods to indicate that <c>WriteCompleted</c> should not be called on success.
-    /// </summary>
-    public sealed class CallbackOnErrorsOnly { }
-}
