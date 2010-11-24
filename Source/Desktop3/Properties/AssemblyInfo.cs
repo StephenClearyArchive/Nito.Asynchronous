71fa73b14e2a233bb8752dcb09d450a1e61d8595
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-11-24 14:46:02 +0000

    Fixed svn folder restructuring.

---
+++ Source/Desktop3/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Nito Programs">
+//     Copyright (c) 2009 Nito Programs.
+// </copyright>
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helper classes for asynchronous programming")]
+[assembly: AssemblyCompany("Nito Programs")]
+[assembly: AssemblyProduct("Nito Libraries")]
+[assembly: AssemblyCopyright("Copyright © Nito Programs 2009")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.4.0.0")]
+
