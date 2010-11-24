5576ec0c70062496d5f092baf2edd463a8433d7e
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-11-24 14:48:41 +0000

    Fixed copyright statements and added target platform in assembly title.

--- Source/Desktop3/Properties/AssemblyInfo.cs
+++ Source/Desktop3/Properties/AssemblyInfo.cs
@@ -1,18 +1,18 @@
 ﻿// <copyright file="AssemblyInfo.cs" company="Nito Programs">
-//     Copyright (c) 2009 Nito Programs.
+//     Copyright (c) 2009-2010 Nito Programs.
 // </copyright>
 
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Helper classes for asynchronous programming")]
+[assembly: AssemblyTitle("Helper classes for asynchronous programming (Desktop 3.5 Client)")]
 [assembly: AssemblyCompany("Nito Programs")]
 [assembly: AssemblyProduct("Nito Libraries")]
-[assembly: AssemblyCopyright("Copyright © Nito Programs 2009")]
+[assembly: AssemblyCopyright("Copyright © Nito Programs 2009-2010")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 
